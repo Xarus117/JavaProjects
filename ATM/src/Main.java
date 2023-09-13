98fef4aac357d2da0a841b661191a1beabfa1d80
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-13 17:08:58 +0200

    Try Catch elección operación
    Se ha añadido un Try Catch para comprobar si se introduce un valor válido para el menú de elección de operación.

--- ATM/src/Main.java
+++ ATM/src/Main.java
@@ -1,33 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner keyboard = new Scanner(System.in);
-		
-		int input;
-		
+
+		int input = 0;
+		boolean checkError = false;
+
 		do {
-			
-			System.out.println("Welcome to ATM. How can we serve you?\n"
-					+ "Deposit: 1\nWithDraw: 2\nCount: 3\nExit: 4");
-			
-			input = keyboard.nextInt();
-			
-			if (input == 1) {
-				System.out.println(1);
-			
-			} else if (input == 2) {
-				System.out.println(2);
-				
-			} else if (input == 3) {
-				System.out.println(3);
-				
+			System.out.println("Welcome to ATM. How can we serve you?\n" + "Deposit: 1\nWithDraw: 2\nCount: 3\nExit: 4");
+			while (!checkError) {
+				try {
+					input = keyboard.nextInt();
+					checkError = true;
+				} catch (InputMismatchException e) {
+					System.out.println("Valor invalido, vuelve a intentarlo:");
+					keyboard.nextLine();
+				}
 			}
 			
+			checkError = false;
+
+			if (input == 1) {
+				System.out.println(1);
+			} else if (input == 2) {
+				System.out.println(2);
+
+			} else if (input == 3) {
+				System.out.println(3);
+
+			}
+
 		} while (input != 4);
-		
+
 		System.out.println("Good bye!");
 	}
 
