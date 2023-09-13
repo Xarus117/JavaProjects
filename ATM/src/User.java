30c529d5ad820c1db7aff6533faf6b117a6f3449
Cezary Gebczyk <74736356+Xarus117@users.noreply.github.com>
2023-09-13 18:17:52 +0200

    Registo de Usuario desde la clase User
    He implementado una método para que los clientes puedan registrarse con un nuevo Usuario, falta realizar validaciones sobre el DNI. Las validaciones de fecha funcionan perfectamente y la del correo electrónico solo detecta si hay una @ así que se puede mejorar.

--- ATM/src/User.java
+++ ATM/src/User.java
@@ -1,14 +1,15 @@
 import java.sql.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class User {
 	String ID; // DOCUMENTO NACIONAL DE IDENTIFICACIÓN o EXTRANJERO
 	String firstName;
-	String lastName; 
+	String lastName;
 	Date dateOfBirth;
 	String address;
 	String email;
-	
-	
+
 	public User(String ID, String firstName, String lastName, Date age, String address, String email) {
 		this.ID = ID;
 		this.firstName = firstName;
@@ -17,57 +18,143 @@ public class User {
 		this.address = address;
 		this.email = email;
 	}
-	
+
 	// GETTERS
-	
-	public String getID()  {
+
+	public String getID() {
 		return ID;
 	}
-	
-	public String getFirstName()  {
+
+	public String getFirstName() {
 		return firstName;
 	}
-	
-	public String getLastName()  {
+
+	public String getLastName() {
 		return lastName;
 	}
-	
-	public Date getDateOfBirth()  {
+
+	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
-	
-	public String getAddress()  {
+
+	public String getAddress() {
 		return address;
 	}
-	
-	public String getEmail()  {
+
+	public String getEmail() {
 		return email;
 	}
-	
+
 	// SETTERS
-	
+
 	public void setID(String ID) {
 		this.ID = ID;
 	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-	
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
+	// REGISTER
+
+	public static User registerUser() {
+		boolean checkError = false;
+		User User = new User(null, null, null, null, null, null);
+		Scanner input = new Scanner(System.in);
+		while (!checkError) {
+			try {
+				System.out.println("Número de identificación:");
+				User.setID(input.nextLine());
+				checkError = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Valor invalido, vuelve a intentarlo:");
+				input.nextLine();
+			}
+		}
+		checkError = false;
+		while (!checkError) {
+			try {
+				System.out.println("Nombre:");
+				User.setFirstName(input.nextLine());
+				checkError = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Valor invalido, vuelve a intentarlo:");
+				input.nextLine();
+			}
+		}
+		checkError = false;
+		while (!checkError) {
+			try {
+				System.out.println("Apellido(s):");
+				User.setLastName(input.nextLine());
+				checkError = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Valor invalido, vuelve a intentarlo:");
+				input.nextLine();
+			}
+		}
+		checkError = false;
+		while (!checkError) {
+			try {
+				System.out.println("Fecha de nacimiento (formato AÑO-MES-DÍA):");
+				String DateHolder = input.nextLine();
+				if (DateHolder.indexOf('-') != 4 && DateHolder.lastIndexOf('-') != -1) {
+					checkError = false;
+					System.out.println("Formato de fecha incorrecto");
+				} else {
+					User.setDateOfBirth(Date.valueOf(DateHolder));
+					checkError = true;
+				}
+			} catch (IllegalArgumentException e) {
+				System.out.println("Valor invalido, vuelve a intentarlo:");
+			}
+		}
+
+		checkError = false;
+		while (!checkError) {
+			try {
+				System.out.println("Localidad / Dirección:");
+				User.setAddress(input.nextLine());
+				checkError = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Valor invalido, vuelve a intentarlo:");
+				input.nextLine();
+			}
+		}
+		checkError = false;
+		while (!checkError) {
+			try {
+				System.out.println("Correo electrónico:");
+				User.setEmail(input.nextLine());
+				if (User.getEmail().indexOf('@') == -1) {
+					System.out.println("Tienes que introducir un correo electrónico válido, vuelva a intentarlo:");
+					checkError = false;
+				} else {
+					checkError = true;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Valor invalido, vuelve a intentarlo:");
+				input.nextLine();
+			}
+		}
+		checkError = false;
+		return User;
+	}
+
 }
